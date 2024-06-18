52c2ded415c9fa95dab3586c538324ab5fb73cef
Muhammad Nasir Bilal <naasrbilal@gmail.com>
2024-06-19 04:53:21 +0500

    Implemented Pea Shooting Mechanism
    Added basic functionality for pea shooting aligned with peashooter position. Peas move with the player and disappear at screen's end, with new peas generated subsequently.

--- PvZ/Elements/Pea.h
+++ PvZ/Elements/Pea.h
@@ -1,54 +1,53 @@
-
 #pragma once
-#include "iostream"
-#include "../coordinates.h"
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
-
-
+using namespace std;
+using namespace sf;
 class Pea {
-private:
-	sf::Sprite peaSprite;
-	sf::Texture peaTexture;
-	sf::IntRect peaTextureRect;
-	float posX, posY; // X and Y coordinates for position
-	float velX, velY; // X and Y velocities
-
 public:
-	Pea(float startX = 0, float startY = 0, float velocityX = 0, float velocityY = 0)
-		: posX(startX), posY(startY), velX(velocityX), velY(velocityY) {
-		if (!peaTexture.loadFromFile("../SFML/Images/pea.png")) {
-			std::cerr << "Failed to load pea texture!" << std::endl;
-		}
-		peaTextureRect = sf::IntRect(10, 10, 20, 20);
-		peaSprite.setTexture(peaTexture);
-		peaSprite.setTextureRect(peaTextureRect);
-		peaSprite.setPosition(posX, posY);
-	}
+    float x, y;
+    bool exists;
+    sf::Clock peaClock;
+    sf::Sprite sprite;
+    sf::Texture texture; // Add a texture member
 
-	void update(float deltaTime) {
-		posX += velX * deltaTime;
-		posY += velY * deltaTime;
-		peaSprite.setPosition(posX, posY);
-	}
+    Pea() : x(0), y(0), exists(false) {
+        if (!texture.loadFromFile("pea.png")) {
+            // Handle error (texture loading failed)
+            cout << "not loaded!!!" << endl;
+        }
+        sprite.setTexture(texture);
+        sprite.setTextureRect(sf::IntRect(0, 0, 28, 28));
+    }
 
-	void render(sf::RenderWindow& window) {
-		window.draw(peaSprite);
-	}
+    void shoot(float plantX, float plantY) {
+        if ( !exists) {
+            y = plantY+10;
+            x = plantX+80;
+            exists = true;
+        }
+    }
 
-	float getX() const {
-		return posX;
-	}
+    void move() {
+        cout << "////////Pea Clock: " << peaClock.getElapsedTime().asSeconds() << endl;
 
-	float getY() const {
-		return posY;
-	}
+        if (exists && peaClock.getElapsedTime().asMilliseconds() < .0115f )
+        {
+            return;
+        }
 
-	void setPosition(float x, float y) {
-		posX = x;
-		posY = y;
-		peaSprite.setPosition(posX, posY);
-	}
+       peaClock.restart();
+        x += 5;
+
+        if (x > 1198)
+            exists = false;
+    }
+
+    void draw(sf::RenderWindow& window) {
+        if (exists) {
+            sprite.setPosition(x, y);
+            window.draw(sprite);
+        }
+    }
 };
+
